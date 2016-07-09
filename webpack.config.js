3f9f87971d5a6f7c1929a83f9d703834ce3b0f14
Raj Shah <brainix@gmail.com>
2016-07-09 03:34:17 -0700

    Configure webpack
    Build the bundle/map with: `$ ./node_modules/.bin/webpack -d`

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/*---------------------------------------------------------------------------*\
+ |  webpack.config.js                                                        |
+ |                                                                           |
+ |  Copyright © 2016, Rajiv Bakulesh Shah, original author.                  |
+ |                                                                           |
+ |      This program is free software: you can redistribute it and/or modify |
+ |      it under the terms of the GNU General Public License as published by |
+ |      the Free Software Foundation, either version 3 of the License, or    |
+ |      (at your option) any later version.                                  |
+ |                                                                           |
+ |      This program is distributed in the hope that it will be useful, but  |
+ |      WITHOUT ANY WARRANTY; without even the implied warranty of           |
+ |      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU    |
+ |      General Public License for more details.                             |
+ |                                                                           |
+ |      You should have received a copy of the GNU General Public License    |
+ |      along with this program.  If not, see:                               |
+ |          <http://www.gnu.org/licenses/>                                   |
+\*---------------------------------------------------------------------------*/
+
+
+
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
+
+var config = {
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    }
+};
+
+module.exports = config;
