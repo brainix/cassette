ef5761a04f288ca61111b4928258538e54e2b48a
Raj Shah <brainix@gmail.com>
2016-07-12 23:29:06 -0700

    Hello, World! (From React)
    npm install react and react-dom with: `$ npm i react react-dom -S`
    
    Update the bundle whenever the files change with: `$
    ./node_modules/.bin/webpack -d`

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -20,4 +20,13 @@
 
 
 
-console.log('Hello, World!');
+import React from 'react';
+import {render} from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return <p>Hello, World!</p>;
+    }
+}
+
+render(<App/>, document.getElementById('app'));
