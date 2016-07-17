4e27cea7ee2fb5a17ea71eb80e7bae6c6f061d6e
Raj Shah <brainix@gmail.com>
2016-07-16 18:27:46 -0700

    Simplify project structure

--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+const BUILD_DIR = path.resolve(__dirname, 'public');
+const APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
     entry: APP_DIR + '/index.jsx',
