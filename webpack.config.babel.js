2f847500b2da67e4683e2f64a5cddb44db2ecc1a
Rajiv Bakulesh Shah <brainix@gmail.com>
2017-02-04 04:17:05 -0800

    Update dependencies

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -62,8 +62,8 @@ module.exports = {
                 test: /\.scss$/,
                 include: SRC_DIR,
                 loader: ExtractTextPlugin.extract({
-                    fallbackLoader: 'style-loader',
-                    loader: 'css-loader!postcss-loader!sass-loader',
+                    fallback: 'style-loader',
+                    use: 'css-loader!postcss-loader!sass-loader',
                 }),
             },
             {
